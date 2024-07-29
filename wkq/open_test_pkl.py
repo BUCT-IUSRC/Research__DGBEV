93327e6b9af8da3c4541b6453b18becba60ac073
think-twice-1218 <82155690+think-twice-1218@users.noreply.github.com>
2024-07-29 09:49:09 +0800

    Delete wkq directory

--- wkq/open_test_pkl.py
+++
@@ -1,7 +0,0 @@
-import pickle
-# f = open('data/nuscenes/nuscenes_infos_val.pkl','rb')
-f = open('/home/dell/wkq/BEVFusion-mit/run/full/official_mvp/test.pkl','rb')
-data = pickle.load(f)
-# f1 = open('/media/dell/hdd01/nuscenes/nuscenes/nuscenes_infos_val.pkl','rb')
-# data1 = pickle.load(f1)
-print(len(data))
